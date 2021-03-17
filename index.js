6a4a37ee695879df26842d425fbb8929a5dc6411
Diego Pereira Lopes <diegoplopes5@gmail.com>
2021-03-17 18:26:52 -0300

    feat: add knight's tour first attempt

---
+++ index.js
@@ -0,0 +1,111 @@
+const initialPosition = process.argv.slice(2)[0]
+
+const initMatrix = (ysize, xsize, fill = 0) => {
+  var matrix = []
+  for (var i = 0; i < ysize; i++) {
+    matrix[i] = []
+    for (var j = 0; j < xsize; j++) {
+      matrix[i][j] = fill
+    }
+  }
+  return matrix
+}
+
+const copyArray = (arr) => arr.slice()
+
+const copyMatrix = (matrix) => matrix.map(copyArray)
+
+const fillMatrixIdx = (matrix, yIdx, xIdx, value = 1) => {
+  const copy = copyMatrix(matrix)
+  copy[yIdx][xIdx] = value
+  return copy
+}
+
+const mapMatrixColumn = (letter) => {
+  const map = {
+    a: 0,
+    b: 1,
+    c: 2,
+    d: 3,
+    e: 4,
+    f: 5,
+    g: 6,
+    h: 7,
+  }
+
+  if (letter && [0, 1, 2, 3, 4, 5, 6, 7].includes(map[letter])) return map[letter]
+  throw new Error('Invalid column: ' + letter)
+}
+
+const mapBoardColumn = (matrixColumn) => {
+  if (matrixColumn < 8) return ['a','b','c','d','e','f','g','h'][matrixColumn]
+  throw new Error('Invalid column: ' + matrixColumn)
+}
+
+const mapMatrixLine = (stringLineNumber) => {
+  const line = Number(stringLineNumber)
+  if (line && line > 0 && line <= 8) return [7, 6, 5, 4, 3, 2, 1, 0][line - 1]
+  throw new Error('Invalid line: ' + stringLineNumber)
+}
+
+const mapBoardLine = (matrixLine) => {
+  if (matrixLine > -1 && matrixLine < 8) return [8, 7, 6, 5, 4, 3, 2, 1][matrixLine]
+  throw new Error('Invalid line: ' + matrixLine)
+}
+
+const getHorsePossibleMoves = (currLine, currColumn) => ([
+  { x: currColumn - 2, y: currLine - 1 },
+  { x: currColumn - 1, y: currLine - 2 },
+  { x: currColumn + 1, y: currLine - 2 },
+  { x: currColumn + 2, y: currLine - 1 },
+  { x: currColumn + 2, y: currLine + 1 },
+  { x: currColumn + 1, y: currLine + 2 },
+  { x: currColumn - 1, y: currLine + 2 },
+  { x: currColumn - 2, y: currLine + 1 },
+])
+
+const getNextMove = (board, possibleMoves) => {
+  const calculate = (board, moves, attempt) => {
+    if(attempt > 7) return
+    else {
+      const { x, y } = moves[attempt]
+      if(board[y] && board[y][x] === 0) return { x, y }
+      else return calculate(board, moves, attempt + 1)
+    }
+  }
+  return calculate(board, possibleMoves, 0)
+}
+
+const getMoves = (board, line, column, acc = { moves: [], board: board}) => {
+  const nextMove = getNextMove(board, getHorsePossibleMoves(line, column))
+  if(!nextMove) return acc
+  else {
+    const boardColumn = mapBoardColumn(column)
+    const boardLine = mapBoardLine(line)
+    const newBoard = fillMatrixIdx(board, nextMove.y, nextMove.x)
+    return getMoves(
+      newBoard,
+      nextMove.y,
+      nextMove.x,
+      {
+        board: newBoard,
+        moves: acc.moves.concat([boardColumn + boardLine.toString()])
+      }
+    )
+  }
+}
+
+const main = () => {
+  let board = initMatrix(8, 8)
+  const column = mapMatrixColumn(initialPosition[0]) // x 
+  const line = mapMatrixLine(initialPosition[1])  // y
+  
+  board = fillMatrixIdx(board, line, column)
+  
+  const result = getMoves(board, line, column)
+
+  console.log(result.moves)
+  console.log(result.board)
+}
+
+main()
